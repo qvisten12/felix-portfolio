b44128f6a38816e478ec9c7b32d24ed3d8fbcb76
Felix Ljungqvist <felixljungqvist97@gmail.com>
2023-08-20 20:13:14 +0200

    Fix styling mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
   const changedPathName = removeSlashesAndNumbers(pathname);
 
   return pathname === "/" ? null : (
-    <header className="flex justify-between mx-auto max-w-screen-2xl pt-20 px-10">
+    <header className="flex justify-between mx-auto max-w-screen-2xl lg:pt-20 pt-10 px-10">
       <h1 className="text-3xl">{changedPathName}</h1>
       <Link href="/" className="text-3xl">
         &#10005;
