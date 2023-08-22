52b1345fafc7ecb12ba309f4377916938e64ba61
Felix Ljungqvist <felixljungqvist97@gmail.com>
2023-08-22 22:08:19 +0200

    Safari check for blob background animation

--- components/AnimatedBlob.tsx
+++ components/AnimatedBlob.tsx
@@ -1,29 +1,49 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AnimatedBlob = () => {
+  const [isSafari, setIsSafari] = useState(false);
   /* -- Glow effect -- */
 
   const blobRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    window.onpointermove = (event) => {
-      const { clientX, clientY } = event;
-
-      blobRef?.current?.animate(
-        {
-          left: `${clientX}px`,
-          top: `${clientY}px`,
-        },
-        { duration: 3000, fill: "forwards" }
-      );
+    const isTouchDevice = () => {
+      if (window) {
+        return (
+          "ontouchstart" in window ||
+          navigator.maxTouchPoints > 0 ||
+          // @ts-ignore
+          navigator.msMaxTouchPoints > 0
+        );
+      }
     };
+
+    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+
+    if (!isTouchDevice() && !isSafari) {
+      window.onpointermove = (event) => {
+        const { clientX, clientY } = event;
+
+        blobRef?.current?.animate(
+          {
+            left: `${clientX}px`,
+            top: `${clientY}px`,
+          },
+          { duration: 3000, fill: "forwards" }
+        );
+      };
+    }
   }, []);
 
   return (
     <>
-      <div ref={blobRef} id="blob"></div>
-      <div id="blur"></div>
+      {isSafari ? null : (
+        <>
+          <div ref={blobRef} id="blob"></div>
+          <div id="blur"></div>
+        </>
+      )}
     </>
   );
 };
